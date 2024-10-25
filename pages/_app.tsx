4ed482c4f110ebeb111b5b94c74fbec06a2e1ede
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2024-10-26 10:02:39 +1030

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 };
 
 export default App;
