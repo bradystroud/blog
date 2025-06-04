ad7105626d706f88f7f0f89502ae920c3dbab576
Brady Stroud [SSW] <bradystroud@me.com>
2025-06-05 09:37:49 +1000

    Add pagination for blogs

--- pages/blogs/page/[page].tsx
+++ pages/blogs/page/[page].tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { Container } from "../../components/util/container";
-import { Section } from "../../components/util/section";
-import { client } from "../../tina/__generated__/client";
-import { Layout } from "../../components/layout";
-import { Blogs } from "../../components/posts/blogs";
-import { Pagination } from "../../components/util/pagination";
+import { Container } from "../../../components/util/container";
+import { Section } from "../../../components/util/section";
+import { client } from "../../../tina/__generated__/client";
+import { Layout } from "../../../components/layout";
+import { Blogs } from "../../../components/posts/blogs";
+import { Pagination } from "../../../components/util/pagination";
 import { InferGetStaticPropsType } from "next";
 import Head from "next/head";
 
 const POSTS_PER_PAGE = 5;
 
-export default function BlogsIndex(
+export default function BlogsPage(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
   const blogs = props.data.blogConnection.edges ?? [];
   const { currentPage, totalPages } = props;
-  const pageTitle = "Brady Stroud | Blog";
+  const pageTitle = `Brady Stroud | Blog - Page ${currentPage}`;
 
-  // Sort blogs by date (newest first)
   const sortedBlogs = [...blogs].sort((a, b) => {
     const dateA = new Date(a?.node?.date || 0);
     const dateB = new Date(b?.node?.date || 0);
@@ -34,7 +33,7 @@ export default function BlogsIndex(
         <meta property="og:title" content={pageTitle} />
         <link
           rel="canonical"
-          href="https://bradystroud.dev/blogs"
+          href={`https://bradystroud.dev/blogs/page/${currentPage}`}
           key="canonical"
         />
       </Head>
@@ -44,8 +43,7 @@ export default function BlogsIndex(
             <h1 className="text-4xl title-font mb-4">Blogs</h1>
           </div>
           <p className="text-gray-600 text-lg pb-5 italic">
-            Learning from your mistakes is cool, but if you can learn from
-            other peoples mistakes, that's even cooler. Thats why you should read my blogs - a collection of past learnings.
+            Learning from your mistakes is cool, but if you can learn from other peoples mistakes, that's even cooler. Thats why you should read my blogs - a collection of past learnings.
           </p>
           <Blogs data={paginatedBlogs} />
           <Pagination currentPage={currentPage} totalPages={totalPages} />
@@ -55,15 +53,36 @@ export default function BlogsIndex(
   );
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps = async ({ params }) => {
   const tinaProps = await client.queries.blogPageQuery();
   const totalPosts = tinaProps.data.blogConnection.edges.length;
   const totalPages = Math.ceil(totalPosts / POSTS_PER_PAGE);
+  const currentPage = parseInt(params.page as string, 10);
+
+  if (currentPage < 1 || currentPage > totalPages) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       ...tinaProps,
-      currentPage: 1,
+      currentPage,
       totalPages,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const tinaProps = await client.queries.blogPageQuery();
+  const totalPosts = tinaProps.data.blogConnection.edges.length;
+  const totalPages = Math.ceil(totalPosts / POSTS_PER_PAGE);
+
+  const paths = Array.from({ length: totalPages - 1 }, (_, i) => ({
+    params: { page: String(i + 2) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
   };
 };
