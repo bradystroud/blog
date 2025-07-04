60f2103993015fb21c9e9944829db545595e151d
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2025-07-04 11:19:49 +1000

    Fix blog list anchors (#50)
    Use anchor tags for blog listing items

--- components/posts/blogs.tsx
+++ components/posts/blogs.tsx
@@ -24,7 +24,7 @@ export const Blogs = ({ data }) => {
             passHref
             legacyBehavior
           >
-            <div
+            <a
               key={post.id}
               className="group block px-6 sm:px-8 md:px-10 py-10 mb-8 last:mb-0 bg-gray-50 bg-linear-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-1000 rounded-md shadow-xs transition-all duration-150 ease-out hover:shadow-md hover:to-gray-50 dark:hover:to-gray-800 hover:cursor-pointer"
             >
@@ -48,7 +48,7 @@ export const Blogs = ({ data }) => {
                   </>
                 )}
               </div>
-            </div>
+            </a>
           </Link>
         );
       })}
