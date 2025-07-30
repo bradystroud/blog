1f1385e9e044a23ebc7cd175bf88c1e8d77a0b53
Brady Stroud [SSW] <bradystroud@me.com>
2025-07-30 20:17:22 +1000

    Fix header nav spacing

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -65,12 +65,16 @@ export const Header = ({ data }) => {
                     <Link
                       key={i}
                       href={"/" + item.href}
-                      className={`block mt-4 md:inline-block md:mt-0 hover:text-blue-600 mr-4 ${
-                        activeItem ? "opacity-50" : ""
-                      }`}
+                      passHref
                       legacyBehavior
                     >
-                      {item.label}
+                      <a
+                        className={`block mt-4 md:inline-block md:mt-0 hover:text-blue-600 mr-4 ${
+                          activeItem ? "opacity-50" : ""
+                        }`}
+                      >
+                        {item.label}
+                      </a>
                     </Link>
                   );
                 })}
