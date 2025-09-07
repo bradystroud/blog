640879ac53f7a70cd03d6600fd9623ff3391e9c0
Brady Stroud [SSW] <bradystroud@ssw.com.au>
2025-09-07 12:23:27 +1000

    Remove AI blog component and related cache management functionality

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,31 +1,7 @@
 import { Hero } from "../components/blocks/hero";
 import { Layout } from "../components/layout";
-import AIBlog from "../components/ai-blog";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 export default function FourOhFour() {
-  const router = useRouter();
-  const [isBlogPath, setIsBlogPath] = useState<boolean>(false);
-  const [blogTitle, setBlogTitle] = useState<string>("");
-
-  useEffect(() => {
-    // Check if the current path looks like a blog post
-    const path = router.asPath;
-    const blogMatch = path.match(/^\/blogs\/(.+)$/);
-
-    if (blogMatch) {
-      setIsBlogPath(true);
-      setBlogTitle(blogMatch[1]);
-    }
-  }, [router.asPath]);
-
-  // If this is a blog path that doesn't exist, render AI blog instead of 404
-  if (isBlogPath && blogTitle) {
-    return <AIBlog title={blogTitle} globalData={null} />;
-  }
-
-  // Regular 404 page for non-blog paths
   return (
     <Layout>
       <Hero
